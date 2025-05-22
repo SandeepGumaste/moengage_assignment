67d871c7dc219d2f631515b125c17ccf38c5db90
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2025-05-23 03:28:56 +0530

    fixes

--- app/api/protected/example/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { withAuth } from '@/lib/api-middleware';
-
-export const GET = withAuth(async (userId, req) => {
-    return NextResponse.json({
-        message: 'This is a protected route',
-        userId: userId
-    });
-});
