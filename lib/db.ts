2bc5c296cf810f5f2c9c246d046b6591113c7e02
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2025-05-22 17:35:54 +0530

    db connection fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,40 +1,35 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=MyCluster`!;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority&appName=MyCluster`;
 
 if (!MONGODB_URI) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env');
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
+const connection = { isConnected: false };
 
 async function connectDB() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-            return mongoose;
-        });
+    if (connection.isConnected) {
+        return;
     }
 
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
+        const db = await mongoose.connect(MONGODB_URI, {
+            bufferCommands: false,
+        });
+        
+        connection.isConnected = db.connections[0].readyState === 1;
+        
+        mongoose.connection.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+            connection.isConnected = false;
+        });
 
-    return cached.conn;
+        return db;
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
 }
 
 export default connectDB;
